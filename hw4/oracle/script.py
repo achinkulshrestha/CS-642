37074da1b11c588c1e6f5d1fd3c1df05ee431920
Achin Kulshrestha <achink@wasabi.cs.wisc.edu>
2014-12-03 19:33:46 -0600

    Added getopt support

--- hw4/oracle/script.py
+++ hw4/oracle/script.py
@@ -1,14 +1,23 @@
+#!/usr/bin/python
 '''
 Created on Nov 26, 2014
 
 @author: Achin
 '''
+
 from subprocess import Popen, PIPE
 import sys
 from operator import pos
+import getopt
 keyfile=""
 hexCiphertext = ""
 dic=[]
+
+def usage():
+    print "Achin Kulshrestha - 9071759469"
+    print "USAGE:python script.py -c <CipherTextFile/HexMessage> -k <KeyFileName - Optional>\n"
+    sys.exit(0)
+
 def convertStrToList(str1):
     cipher = []
     i=0
@@ -18,12 +27,36 @@ def convertStrToList(str1):
     #print cipher
     return cipher
 def main():
-    if(len(sys.argv) > 2):
-        keyfile = sys.argv[1]
-    if(len(sys.argv[2]) > 1):
-        f = open( sys.argv[2], 'r')
-        hexCiphertext = f.read()
-    
+    print "Starting Padding Oracler"
+    keyfile = "keyfile"
+    print "The default keyfile name is - keyfile, this should be in the current directory"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hc:k:", ["cipherText=","KeyFileName="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    if(len(opts) < 1):
+        usage()
+    for o,a in opts:
+        if o == "-k":
+            keyfile = a
+            print "Aha...got keyfile name as input, new keyfile name %s" % keyfile
+        elif o == "-c":
+            hexCiphertext = a
+            try:
+                f = open(a, 'r')
+                if(f):
+                    hexCiphertext = f.read()
+            except Exception as e:
+                pass
+        elif o == "-h":
+            usage()
+        else:
+            print "Unhandled Option"
+            usage()
+                                                           
     iv = []
     interB =[]
     for lenIV in range(0,16):
@@ -51,17 +84,21 @@ def main():
             finalText = firstBlock+firstByte+tag
             #finalText = struct.pack('<16B16B32B',firstBlock,first,tag)
             #print finalText
-            proc = Popen(["./baddecrypt.py",keyfile,finalText],stdout=PIPE,stderr=PIPE)
-            output = proc.communicate()[0]
+            try:
+                proc = Popen(["./baddecrypt.py",keyfile,finalText],stdout=PIPE,stderr=PIPE)
+                output = proc.communicate()[0]
+            except Exception as e:
+                print "Exception %s" % e
+
             #print proc
             #print i
-            #print output
             if(output.find("Tag") != -1):                
                 #print "intermediate Byte "
                 iByte =  val ^ i
                 #print iByte
                 interB[pos] = '{0:02x}'.format(iByte)
                 sys.stdout.write(('{0:02x}'.format(int(ivList[pos],16) ^ iByte)).decode('hex'))
+                sys.stdout.flush()
                 dic.append('{0:02x}'.format(int(ivList[pos],16) ^ iByte))
                 #print dic
                 break
